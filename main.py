fbc6c6a876bab62f049d2929d0fb146072e63519
Rick <31972051+searri@users.noreply.github.com>
2020-11-04 22:19:11 -0600

    main runs

--- main.py
+++ main.py
@@ -56,51 +56,51 @@ class TAPModel(object):
         self.N = len(self.G.nodes())
         self.M = len(self.G.edges())
 
-        print 'Loaded graph with {0} nodes and {1} edges.'.format(self.N, self.M)
+        print('Loaded graph with {0} nodes and {1} edges.'.format(self.N, self.M))
         
         self.T = self.theta.shape[1]
         self.N_d = self.theta.shape[0]
 
-        self.yold = { i: {k:0 for k in xrange(self.T) } for i in self.G.nodes() }
+        self.yold = { i: {k:0 for k in range(self.T) } for i in self.G.nodes() }
 
-        print 'Loaded distributions over {0} topics for {1} nodes.'.format(self.T, self.N_d)        
+        print('Loaded distributions over {0} topics for {1} nodes.'.format(self.T, self.N_d))        
         
         #	1.1 calculate g(vi,yi,z)
         self._calculate_g()
-        print 'Calculated g'
+        print('Calculated g')
 
         #   1.2 Eq8, calculate bz,ij
         self._calculate_b()
-        print 'Calculated b'        
+        print('Calculated b')        
 
 
     def _calculate_g(self):
         """eq. 1"""
         for i in self.G.nodes():
             n = self.G.neighbors(i)
-            self.g[i] = np.zeros((len(n)+1, self.T))
+            self.g[i] = np.zeros((len(list(n))+1, self.T))
 
             sumin = np.zeros((self.T))
             sumout = np.zeros((self.T))
         
-            for t, attr in self.G[i].iteritems():
+            for t, attr in self.G[i].items():
                 this = int(t) - 1
-                for k in xrange(self.T):
+                for k in range(self.T):
                     w = float(attr['weight'])     
                     sumout[k] = sumout[k] + w * self.theta[this,k]
 
-            for t, attr in self.G[i].iteritems():
-                for k in xrange(self.T):
+            for t, attr in self.G[i].items():
+                for k in range(self.T):
                     w = float(attr['weight'])
                     this = int(i) - 1                
                     sumin[k] = sumin[k] + w * self.theta[this,k]
                 
                     # calculate y z, i=i ;; [n,] should be the last row.
-                    self.g[i][len(n),k] = sumin[k] / (sumin[k] + sumout[k])
+                    self.g[i][len(list(n)),k] = sumin[k] / (sumin[k] + sumout[k])
                 
             j = 0
-            for t,attr in self.G[i].iteritems():
-                for k in xrange(self.T):
+            for t,attr in self.G[i].items():
+                for k in range(self.T):
                     w = float(attr['weight'])
                     this = int(t) - 1
                     self.g[i][j,k] = w * self.theta[this,k] / (sumin[k] + sumout[k])
@@ -110,17 +110,17 @@ class TAPModel(object):
         """eq. 8"""
         for i in self.G.nodes():
             n = self.G.neighbors(i)
-            self.b[i] = np.zeros((len(n)+1, self.T))
-            self.r[i] = np.zeros((len(n)+1, self.T))
-            self.a[i] = np.zeros((len(n)+1, self.T))
+            self.b[i] = np.zeros((len(list(n))+1, self.T))
+            self.r[i] = np.zeros((len(list(n))+1, self.T))
+            self.a[i] = np.zeros((len(list(n))+1, self.T))
             
             sum_ = np.zeros((self.T))
         
-            for j in xrange(len(n)+1):   # +1 to include self.
-                for k in xrange(self.T):
+            for j in range(len(list(n))+1):   # +1 to include self.
+                for k in range(self.T):
                     sum_[k] += self.g[i][j,k]
-            for j in xrange(len(n)+1):
-                for k in xrange(self.T):
+            for j in range(len(list(n))+1):
+                for k in range(self.T):
                     self.b[i][j,k] = np.log(self.g[i][j,k] / sum_[k])
 
     def _update_r(self):
@@ -134,20 +134,25 @@ class TAPModel(object):
             temp = 0.
             maxk = {}
         
-            if len(n) < 1:  # node has no neighbors
-                for k in xrange(self.T):
+            if len(list(n)) < 1:  # node has no neighbors
+                for k in range(self.T):
                     self.r[i][0,k] = self.b[i][0,k]
             else:
-                for k in xrange(self.T):
+                for k in range(self.T):
                     firstmax[k] = self.b[i][0,k] + self.a[i][0,k]
-                    secondmax[k] = self.b[i][1,k] + self.a[i][1,k]
+
+                    try:
+                        secondmax[k] = self.b[i][1,k] + self.a[i][1,k]
+                    except IndexError:
+                        secondmax[k] = self.b[i][1,k]
+
                     maxk[k] = 0
                     if secondmax[k] > firstmax[k]:
                         firstmax[k], secondmax[k] = swap(firstmax[k], secondmax[k])
                         maxk[k] = 1
 
-                for j in xrange(2, len(n)+1):
-                    for k in xrange(self.T):
+                for j in range(2, len(list(n))+1):
+                    for k in range(self.T):
                         temp = self.a[i][j,k] + self.b[i][j,k]
                         if temp > secondmax[k]:
                             temp, secondmax[k] = swap(temp, secondmax[k])
@@ -156,8 +161,8 @@ class TAPModel(object):
                             firstmax[k], secondmax[k] = swap(firstmax[k], secondmax[k])
                             maxk[k] = j
             
-                for j in xrange(len(n) + 1):
-                    for k in xrange(self.T):
+                for j in range(len(list(n)) + 1):
+                    for k in range(self.T):
                         if j == maxk[k]:
                             self.r[i][j,k] = ( (self.b[i][j,k] - secondmax[k]) * (1. - self.damper) ) + ( self.r[i][j,k] * self.damper )
                         else:
@@ -175,37 +180,52 @@ class TAPModel(object):
         
             maxk[j] = np.array( [-1] * self.T )
 
-            n = self.G.neighbors(j)
+            n = list(self.G.neighbors(j))
         
             # maxk[N] records the maximum value of min{r z, kj, 0}
             if len(n) < 1:
-                for k in xrange(self.T):
+                for k in range(self.T):
                     firstmax[j][k] = 0.
         
             else:
                 neighbour = n[0]
-                pos = self.G.neighbors(neighbour).index(j)
+                neighbor_list = list(self.G.neighbors(neighbour))
+                pos = neighbor_list.index(j)
             
-                for k in xrange(self.T):
-                    firstmax[j][k] = min( self.r[neighbour][pos, k], 0. )
+                for k in range(self.T):
+                    try:
+                        firstmax[j][k] = min( self.r[neighbour][pos, k], 0. )
+                    except IndexError:
+                        firstmax[j][k] = 0
+
                     maxk[j][k] = neighbour
             
                 if len(n) >= 2:
                     neighbour = n[1]
-                    pos = self.G.neighbors(neighbour).index(j)
+                    neighbor_list = list(self.G.neighbors(neighbour))
+                    pos = neighbor_list.index(j)
                 
-                    for k in xrange(self.T):
-                        secondmax[j][k] = min( self.r[neighbour][pos, k], 0. )
+                    for k in range(self.T):
+                        try:
+                            secondmax[j][k] = min( self.r[neighbour][pos, k], 0. )
+                        except IndexError:
+                            secondmax[j][k] = 0
+
                         if secondmax[j][k] > firstmax[j][k]:
                             firstmax[j][k], secondmax[j][k] = swap(firstmax[j][k], secondmax[j][k])
                             maxk[j][k] = neighbour
             
-                    for i in xrange(2, len(n)):
+                    for i in range(2, len(n)):
                         neighbour = n[i]
-                        pos = self.G.neighbors(neighbour).index(j)
+                        neighbor_list = list(self.G.neighbors(neighbour))
+                        pos = neighbor_list.index(j)
                     
-                        for k in xrange(self.T):
-                            temp = min ( self.r[neighbour][pos,k] , 0. )
+                        for k in range(self.T):
+                            try:
+                                temp = min ( self.r[neighbour][pos,k] , 0. )
+                            except IndexError:
+                                temp = 0
+
                             if temp > secondmax[j][k]:
                                 temp, secondmax[j][k] = swap(temp, secondmax[j][k])   
                             if secondmax[j][k] > firstmax[j][k]:
@@ -214,12 +234,15 @@ class TAPModel(object):
 
         for i in self.G.nodes():
             n = self.G.neighbors(i)
-            for k in xrange(self.T): # a_ii
-                self.a[i][len(n), k] = firstmax[i][k]
+            for k in range(self.T): # a_ii
+                try:
+                    self.a[i][len(list(n)), k] = firstmax[i][k]
+                except IndexError:
+                    self.a[i][k] = firstmax[i][k]
         
             for j in n: # a_ij
                 j_index = n.index(j)
-                for k in xrange(self.T):
+                for k in range(self.T):
                     if i == maxk[j][k]:
                         use = secondmax[i][k]
                     else:
@@ -237,13 +260,21 @@ class TAPModel(object):
 
         dc = 0
         for i in self.G.nodes():
-            n = self.G.neighbors(i)
-            for k in xrange(self.T):
-                firstmax = self.r[i][len(n), k] + self.a[i][len(n), k]
+            n = list(self.G.neighbors(i))
+            for k in range(self.T):
+                try:
+                    firstmax = self.r[i][len(n), k] + self.a[i][len(n), k]
+                except IndexError:
+                    firstmax = self.r[i][0, k] + self.a[i][0, k]
+                
                 rep = -1
             
-                for j in xrange(len(n)):
-                    temp = self.r[i][j,k] + self.a[i][j,k]
+                for j in range(len(n)):
+                    try:
+                        temp = self.r[i][j,k] + self.a[i][j,k]
+                    except IndexError:
+                        temp = self.r[i][0,k] + self.a[i][0,k]
+                    
                     if temp > firstmax:
                         temp, firstmax = swap(temp, firstmax)
                         rep = j
@@ -274,20 +305,28 @@ class TAPModel(object):
         self.MU = {}
 
         # Export
-        for k in xrange(self.T):
+        for k in range(self.T):
             subg = nx.DiGraph()
             
             # Influence
             for i in self.G.nodes():
-                n = self.G.neighbors(i)
+                n = list(self.G.neighbors(i))
                 for j in self.G.nodes():
                     if j in n:
                         j_ = n.index(j)
-                        i_ = self.G.neighbors(j).index(i)
+                        j_neighbors = list(self.G.neighbors(j))
+                        i_ = j_neighbors.index(i)
                 
                         # Equation 9.
-                        j_i = 1./ (1. + np.exp(-1. * (self.r[i][j_,k] + self.a[i][j_,k])))
-                        i_j = 1./ (1. + np.exp(-1. * (self.r[j][i_,k] + self.a[j][i_,k])))
+                        try:
+                            j_i = 1./ (1. + np.exp(-1. * (self.r[i][j_,k] + self.a[i][j_,k])))
+                        except:
+                            j_i = 1./ (1. + np.exp(-1. * (self.r[i][0,k] + self.a[i][0,k])))
+                        
+                        try:
+                            i_j = 1./ (1. + np.exp(-1. * (self.r[j][i_,k] + self.a[j][i_,k])))
+                        except:
+                            i_j = 1./ (1. + np.exp(-1. * (self.r[j][0,k] + self.a[j][0,k])))
                 
                         if j_i > i_j:   # Add only strongest edge.
                             subg.add_edge(j, i, weight=float(j_i))
@@ -295,8 +334,10 @@ class TAPModel(object):
                             subg.add_edge(i, j, weight=float(i_j))
                             
             # Theta
+            constant = list(self.G.nodes())
+            c = int(constant[0])
             for i in self.G.nodes():
-                G.node[i]['theta'] = self.theta[i, k]
+                self.G.nodes[i]['theta'] = self.theta[int(i) - c, int(k)]
                 
             self.MU[k] = subg
     
@@ -317,24 +358,31 @@ class TAPModel(object):
         """
         
         for i in alt_G.nodes():
-            alt_n = alt_G.neighbors(i)            
+            alt_n = list(alt_G.neighbors(i))           
             if i in self.G.nodes():
                 # alt_r and alt_a must be from a model with the same topics.
                 assert alt_r[i].shape[1] == self.r[i].shape[1]
                 assert alt_a[i].shape[1] == self.a[i].shape[1]
                 
-                n = self.G.neighbors(i)
+                n = list(self.G.neighbors(i))
                 for j in alt_n:
                     if j in n:
                         j_ = n.index(j)
                         alt_j_ = alt_n.index(j)
 
-                        for k in xrange(self.T):
-                            self.r[i][j_,k] = alt_r[i][alt_j_,k]
-                            self.a[i][j_,k] = alt_a[i][alt_j_,k]                        
+                        for k in range(self.T):
+                            try:
+                                self.r[i][j_,k] = alt_r[i][alt_j_,k]
+                            except IndexError:
+                                self.r[i][0,k] = alt_r[i][0,k]
+                            
+                            try:
+                                self.a[i][j_,k] = alt_a[i][alt_j_,k]
+                            except IndexError:
+                                self.a[i][0,k] = alt_a[i][0,k]
     
     def write(self, target):
-        for k in self.MU.keys():
+        for k in list(self.MU.keys()):
             nx.write_graphml(self.MU[k], '{0}_topic_{1}.graphml'.format(target,k))    
 
     def graph(self, k):
@@ -373,13 +421,13 @@ with open(distpath, 'r') as f:
 
 theta = np.random.rand(234,10)
 
-print 'first model'
+print('first model')
 model = TAPModel(G, theta)
 model.build()
 
 alt_r, alt_a, alt_G = model.r, model.a, model.G
 
-print 'second model'
+print('second model')
 model2 = TAPModel(G, theta)
 model2.prime(alt_r, alt_a, alt_G)
 model2.build()
